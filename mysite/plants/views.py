724664f02bc2d3955f93f9975d873e040def7a91
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-10-20 15:13:28 +0530

    Commit:10

--- mysite/plants/views.py
+++ mysite/plants/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from plants.models import Item
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1 style = "color : Red ">This is an index page</h1>')
+    itemlist = Item.objects.all()
+
+    return HttpResponse(itemlist)
 
 def detail(request):
     return HttpResponse('<h1 style = "color : Orange"> This is a details page</h1>')
