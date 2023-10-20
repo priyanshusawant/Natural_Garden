8521395d2982b8f3473640c02bff2c44f1ff3815
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-10-20 23:50:04 +0530

    Commit:23

--- mysite/plants/views.py
+++ mysite/plants/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from plants.models import Item
+from plants.forms import ItemForm
 
 # Create your views here.
 
@@ -21,3 +22,16 @@ def detail(request, item_id):
     }
 
     return render(request, 'plants/detail.html', context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('plants:index')
+    
+    context = {
+        'form':form
+    }
+
+    return render(request, 'plants/item-form.html', context)
