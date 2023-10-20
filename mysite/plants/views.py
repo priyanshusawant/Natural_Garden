417fcb06476a3ab368b1936d95ce81a02dede3e4
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-10-20 13:29:54 +0530

    Commit:6

--- mysite/plants/views.py
+++ mysite/plants/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is an index page')
+    return HttpResponse('<h1 style = "color : Red ">This is an index page</h1>')
+
+def detail(request):
+    return HttpResponse('<h1 style = "color : Orange"> This is a details page</h1>')
