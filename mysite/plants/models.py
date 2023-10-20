c81883a843e65a6c0411f7d3d30da51fbc5d8c7b
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-10-20 20:51:30 +0530

    Commit:17

--- mysite/plants/models.py
+++ mysite/plants/models.py
@@ -6,6 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500, 
+        default="https://i.pinimg.com/736x/fa/d3/4a/fad34a707359c5c1bfe1a4461010b7a1.jpg"
+    )
 
     def __str__(self):
         return self.item_name
