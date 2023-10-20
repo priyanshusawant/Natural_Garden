2a297aa4404ef8d231e6d80b4faa2362fc959e6a
priyanshusawant29 <106573705+priyanshusawant29@users.noreply.github.com>
2023-10-20 15:44:43 +0530

    Commit:14

--- mysite/plants/urls.py
+++ mysite/plants/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include 
 from plants import views 
 
+app_name = 'plants'
+
 urlpatterns = [
     path('home/', views.index, name='index'),
     path('detail/<int:item_id>', views.detail, name='detail'),
